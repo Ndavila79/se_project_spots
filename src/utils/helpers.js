98abc0b918e686fb6f4e560ad419e64c24c2d03b
unknown <ndandavila79@gmail.com>
2026-06-27 20:04:10 -0500

    fix: delete modal renderLoading text has been fix and media queries in modal.css has been updated

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,13 +1,28 @@
-export function setButtontext (
-  btn,
-  isLoading,
-  defaultText = "Save",
-  loadingText = "Saving...") {
-if(isLoading) {
-btn.textContent = loadingText;
-
-} else {
-btn.textContent = defaultText;
-
+export function renderLoading(isLoading, button, buttonText='Save', loadingText='Saving...') {
+  if (isLoading) {
+    button.textContent = loadingText
+  } else {
+    button.textContent = buttonText
+  }
 }
+
+
+function handleSubmit(request, evt, loadingText = 'Saving...') {
+  evt.preventDefault();
+
+  const submitButton = evt.submitter;
+  const initialText = submitButton.textContent;
+
+  renderLoading(true, submitButton, initialText, loadingText);
+  request()
+    .then(() => {
+
+      evt.target.reset();
+    })
+
+    .catch(console.error)
+
+    .finally(() => {
+      renderLoading(false, submitButton, initialText);
+    });
 }
